f55f08a981e0771aeba796fc90d9f8437703a0c1
Franz Miltz <franz@miltz.me>
2020-09-19 23:54:35 +0100

    Add execute permissions for speedtest

--- src/setup.sh
+++ src/setup.sh
@@ -7,3 +7,4 @@ cd $BUILD_DIR
 wget -O speedtest.tgz https://bintray.com/ookla/download/download_file?file_path=ookla-speedtest-1.0.0-x86_64-linux.tgz
 tar xfz speedtest.tgz
 rm -rf speedtest.*
+chmod +x speedtest
